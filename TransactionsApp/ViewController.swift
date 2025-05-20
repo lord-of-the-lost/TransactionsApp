d0168012b1e1b3f7a83ca896ff45a3f4ea5b0041
lord-of-the-lost <ignatov.99@yandex.ru>
2025-05-20 22:36:06 +0300

    Initial Commit

---
+++ TransactionsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionsApp
+//
+//  Created by Николай Игнатов on 20.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
