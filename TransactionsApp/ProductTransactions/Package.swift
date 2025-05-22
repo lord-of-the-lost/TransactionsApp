a0418cb7a7ba18e71d31c7c587680e9c1dea1e5e
lord-of-the-lost <ignatov.99@yandex.ru>
2025-05-22 14:58:21 +0300

    add ProductTransactions module

---
+++ TransactionsApp/ProductTransactions/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ProductTransactions",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "ProductTransactions",
+            targets: ["ProductTransactions"]),
+    ],
+    dependencies: [
+        .package(path: "../Core")
+    ],
+    targets: [
+        .target(
+            name: "ProductTransactions",
+            dependencies: [.product(name: "Core", package: "Core")]
+        )
+    ]
+)
